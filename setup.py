ef0d704dbaa6b9ed42c5b6a5249fd6bcc127bf84
mudassirahmad <mudassirahmad998@gmail.com>
2023-11-11 15:18:21 -0500

    Making repository pip installable

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup
 setup(
     name='math_quiz',
     version='0.1.0',    
-    author='Stephen Hudson',
+    author='Mudassir Ahmed',
     author_email='mudassir.ahmed@fau.de',
     license='Appache',
     packages=[],
     install_requires=['setuptools==68.2.2'],
     entry_points={
         'console_scripts': [
-            'math_quiz = math_quiz.math_quiz:__main__',
+            'math_quiz = DSSS_H2.math_quiz.math_quiz:__main__',
         ],
     }
 
